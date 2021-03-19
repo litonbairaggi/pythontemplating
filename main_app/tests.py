fe2327210d7fb3647f7b41bd66560f7f5c23e92f
litonbairaggi <litonovi2013@gmail.com>
2021-03-19 18:41:47 +0600

    python templates first commit

---
+++ main_app/tests.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+
+class MainAppTest(SimpleTestCase):
+def test_home_view_exist(self):
+    response = self.client.get('/')
+    self.assertEqual(response.srtatus_code, 200)
+
+def test_about_view_exist(self):
+    response = self.client.get('/about')
+    self.assertEqual(response.srtatus_code, 200)
+
---
+++ main_app/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['name'] = "Liton Bairaggi"
+        context['city'] = "Khulna"
+
+        list=[1,2,3,4,5]
+        context['list']=list
+
+        return context
+
+class AboutView(TemplateView):
+    template_name = "about.html"    
+
+class ContactView(TemplateView):
+    template_name = "contact.html" 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['email'] = "liton@gmail.com"
+        context['phone'] = "01610202717"
+        
+        return context      
