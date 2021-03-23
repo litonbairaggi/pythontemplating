c14ffa43bf237306d4901411ab4cc2c7bfeca03f
litonbairaggi <litonovi2013@gmail.com>
2021-03-24 02:31:50 +0600

    djangosalon project complate all frontend

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('',views.HomeView.as_view(), name="home"),
+    path('/',views.HomeView.as_view(), name="/"),
     path('about/',views.AboutView.as_view(), name="about"),
     path('contact/',views.ContactView.as_view(), name="contact"),
 
     path('blog/',views.BlogView.as_view(), name="blog"),
+    path('blog_grid/',views.Blog_GridView.as_view(), name="blog_grid"),
+    path('blog_list/',views.Blog_ListView.as_view(), name="blog_list"),
+    path('blog_details/',views.Blog_DetailsView.as_view(), name="blog_details"),
+    path('services/',views.ServicesView.as_view(), name="services"),
+    path('services_details/',views.Services_DetailsView.as_view(), name="services_details"),
+    path('gallery_01/',views.GalleryView.as_view(), name="gallery_01"),
+    path('gallery_02/',views.Gallery_02View.as_view(), name="gallery_02"),
+    path('gallery_03/',views.Gallery_03View.as_view(), name="gallery_03"),
+    path('testimonials/',views.TestimonialsView.as_view(), name="testimonials"),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,30 +3,38 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        context['name'] = "Liton Bairaggi"
-        context['city'] = "Khulna"
-
-        list=[1,2,3,4,5]
-        context['list']=list
-
-        return context
-
 class AboutView(TemplateView):
     template_name = "about.html"    
 
 class ContactView(TemplateView):
-    template_name = "contact.html" 
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        context['email'] = "liton@gmail.com"
-        context['phone'] = "01610202717"
-        
-        return context    
+    template_name = "contact.html"  
 
 class BlogView(TemplateView):
-    template_name = "blog.html"             
+    template_name = "blog.html"  
+
+class Blog_GridView(TemplateView):
+    template_name = "blog_grid.html"  
+
+class Blog_ListView(TemplateView):
+    template_name = "blog_list.html"             
+
+class Blog_DetailsView(TemplateView):
+    template_name = "blog_details.html" 
+
+class ServicesView(TemplateView):
+    template_name = "services.html"  
+
+class Services_DetailsView(TemplateView):
+    template_name = "services_details.html"             
+
+class GalleryView(TemplateView):
+    template_name = "gallery.html"         
+
+class Gallery_02View(TemplateView):
+    template_name = "gallery_02.html"             
+
+class Gallery_03View(TemplateView):
+    template_name = "gallery_03.html"             
+
+class TestimonialsView(TemplateView):
+    template_name = "testimonials.html"             
